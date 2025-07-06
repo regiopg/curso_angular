a872f57a332ce725e66e7468c7b97b4e7fee6770
jmpcenkontrol <jpina@enkontrol.com>
2025-07-05 21:23:43 -0600

    Inicio sección 6

---
+++ fernandoHerrera/angularCeroAExperto/seccion6/03-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'trending-page',
+  templateUrl: './trending-page.component.html',
+  styleUrl: './trending-page.component.css'
+})
+export default class TrendingPageComponent {
+
+}
